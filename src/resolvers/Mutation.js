776a4f91634b8effd7ac079937164ce9ba9c3a7d
yhHuang <b04602013@ntu.edu.tw>
2019-06-25 01:39:51 +0800

    fix:passing wrong arguement to mutation
    Signed-off-by: yhHuang <b04602013@ntu.edu.tw>

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -2,7 +2,10 @@ import UserStore from '../class/UserStore/UserStore.class';
 
 const Mutation = {
   login(parent, args) {
-    return UserStore.login(args);
+    return UserStore.login(args.arg);
+  },
+  register(parent, args) {
+    return UserStore.register(args.arg);
   },
 };
 
