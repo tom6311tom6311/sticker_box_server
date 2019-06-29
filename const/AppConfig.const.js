4c33fb63fadd13b3e29b45e18dd3a872f4c35887
yhHuang <b04602013@ntu.edu.tw>
2019-06-29 19:25:30 +0800

    fix: Is-Chinese rule wrong
    Signed-off-by: yhHuang <b04602013@ntu.edu.tw>

--- const/AppConfig.const.js
+++ const/AppConfig.const.js
@@ -9,7 +9,7 @@ const AppConfig = {
     USER_ID: new RegExp('^[a-zA-Z_][a-z0-9]{7,19}$'),
     PASSWORD: new RegExp('^[a-z0-9]{8,20}$'),
     UUID: new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i),
-    CHINESE_ONLY: new RegExp('^[\u4e00-\u9fa5]$'),
+    CHINESE_ONLY: new RegExp('^[\u4e00-\u9fa5]+$'),
   },
   TERM_LIB: {
     STICKER: 'STICKER',
